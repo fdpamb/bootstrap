954c55990078b478820235cdc5a591eea85eee11
Friska (Dendi) Pambudi <friska.pambudi@gridqube.com>
2025-12-22 13:13:16 +1000

    updated to version: 3.4.2

--- js/button.js
+++ js/button.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Bootstrap: button.js v3.4.1
+ * Bootstrap: button.js v3.4.2
  * https://getbootstrap.com/docs/3.4/javascript/#buttons
  * ========================================================================
  * Copyright 2011-2019 Twitter, Inc.
