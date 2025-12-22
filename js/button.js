f50bab5f52a239672e4aa4fc4321e525a6fa0f0a
Friska (Dendi) Pambudi <friska.pambudi@gridqube.com>
2025-12-22 12:50:55 +1000

    sanitised cve-2024-6485 vulnerabilities

--- js/button.js
+++ js/button.js
@@ -28,7 +28,7 @@
   Button.prototype.setState = function (state) {
     var d    = 'disabled'
     var $el  = this.$element
-    var val  = $el.is('input') ? 'val' : 'html'
+    var val  = $el.is('input') ? 'val' : 'text' // Fixed: use .text() to avoid interpreting HTML/script - sanitise cve-2024-6485 vulnerability
     var data = $el.data()
 
     state += 'Text'
